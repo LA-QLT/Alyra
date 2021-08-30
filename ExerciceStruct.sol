a11cdec75b2c277f48a51f42a8d5495381b2073b
Lahcen EL OUARDI <lahcen07elouardi@gmail.com>
2021-08-31 01:01:59 +0200

    Definir un tableau public person

--- ExerciceStruct.sol
+++ ExerciceStruct.sol
@@ -4,10 +4,14 @@ pragma solidity 0.6.11;
 
 contract Whitetlist {
 
+
     struct Person {
         string name;
         uint age;
     }
+
+    Person [] public people; 
+    
     function addPerson(string _name, uint _age){
         Person memory personne = Person(_name, _age);
     }
