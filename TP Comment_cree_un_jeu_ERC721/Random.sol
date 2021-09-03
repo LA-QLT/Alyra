7f2594538c910ed00ca0560d41e8df50babd235e
Lahcen EL OUARDI <lahcen07elouardi@gmail.com>
2021-09-04 00:04:25 +0200

    Generer un nb Aleatoire

---
+++ TP Comment_cree_un_jeu_ERC721/Random.sol
@@ -0,0 +1,14 @@
+// Crowdsale.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+
+contract Random{
+    uint private nonce =0;
+
+    function random() public returns(uint) {
+        nonce++;
+        return uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,nonce))) % 101;
+        
+    }
+}
